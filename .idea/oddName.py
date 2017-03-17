8786875409bac86216167bc4b622ddc59bbcf4b2
Aaron <aaron.lanskey@my.jcu.edu.au>
2017-03-17 14:47:14 +1000

    Add name program

--- .idea/oddName.py
+++ .idea/oddName.py
@@ -1,1 +1,6 @@
 """ Aaron Lanskey"""
+userName = input("Please give your name:\t")
+while userName == "":
+    userName = input("Please give your name:\t")
+for x in range(0,len(userName),2):
+    print(userName[x])
